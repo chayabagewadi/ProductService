02df545d3eb86258d9c9699e440dc2de0fa5e120
ChayaBagewadi <chayabagewadi2124@gmail.com>
2025-06-26 13:31:56 +0530

    Test Path Variables

--- src/main/java/com/myproject/productservice/controllers/SampleController.java
+++ src/main/java/com/myproject/productservice/controllers/SampleController.java
@@ -1,8 +1,6 @@
 package com.myproject.productservice.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Sample")
@@ -11,8 +9,18 @@ public class SampleController {
     public String sample1() {
         return "Hello World";
     }
-    @GetMapping("/func2")
-    public String sample2() {
-        return "Good Night World";
+
+
+    @GetMapping("/func2/{name}")
+    public String sample2(@PathVariable String name) {
+        return "Good Night " + name;
+    }
+    @GetMapping("/func3/{name}/")
+    public String sample3( @RequestParam("x") int x, @PathVariable String name) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < x; i++) {
+            sb.append(name).append(" ");
+        }
+        return sb.toString();
     }
 }
